0413f2eed18765d3e03d73ffa6fc3381c57a14ae
Dkvsb <41582680+Dkvsb@users.noreply.github.com>
2022-03-13 22:33:00 +0100

    httpreqs

--- 05-httpreq/src/App.js
+++ 05-httpreq/src/App.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(() => {
+    setIsLoading(true);
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const transformedData = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedData);
+        setIsLoading(false);
+      }).catch(err => {
+        setError(err.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Press to look for Star Wars Movies</p>}
+        {isLoading && !error && <p>Loading...</p>}
+        {error && <p> {error} </p>}
       </section>
     </React.Fragment>
   );
